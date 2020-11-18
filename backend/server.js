9a58ee0f36d8d55c3e39d30ec36a414117272046
Clinton A <hotcoffeehero@gmail.com>
2020-11-18 17:49:55 +0900

    19 Modeling Our Data

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express  from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
 import products  from './data/products.js'
 
@@ -26,5 +27,5 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT || 5000
 
 app.listen(5000, ()=> {
-    console.log(`She's runnin' in ${process.env.NODE_ENV} mode on ${PORT}`)
+    console.log(`She's runnin' in ${process.env.NODE_ENV} mode on ${PORT}`.brightYellow)
 })
